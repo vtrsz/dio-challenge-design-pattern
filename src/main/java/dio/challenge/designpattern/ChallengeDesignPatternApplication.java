5a1c4b7a5940cda1f05500e1ba598b5bd388599d
vtrsz <vitorsouzaribeiro1@gmail.com>
2023-01-13 11:18:58 -0300

    feat: Spring Base Project

---
+++ src/main/java/dio/challenge/designpattern/ChallengeDesignPatternApplication.java
@@ -0,0 +1,13 @@
+package dio.challenge.designpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeDesignPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeDesignPatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/challenge/designpattern/ChallengeDesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.challenge.designpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeDesignPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
